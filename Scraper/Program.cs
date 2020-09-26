1ef35376a230ec49f4e53e035ff3e89a371fa427
levani vanishvili <levan.vanishvili@gmail.com>
2020-09-26 23:20:20 +0400

    Add project files.

---
+++ Scraper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Reflection.Metadata;
+
+namespace Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
